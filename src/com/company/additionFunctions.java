33c6f5efc5a983926392540b8112fdb00d25ea28
vdanylov <vitaliidanylov1992@gmail.com>
2016-08-10 04:26:13 +0200

    experimental Version bug fixed

--- src/com/company/additionFunctions.java
+++ src/com/company/additionFunctions.java
@@ -12,22 +12,22 @@ public class additionFunctions {
     static Population newPop;
 
     //checking rout e function
-    public static boolean isConnected(double[][] arr, BitArray ch) {
-        boolean res = false;
-        int point = 0;
-        for (int i = 1; i < arr.length; i++) {
-            if (ch.get(i) && (arr[point][i] > 0) && !Double.isInfinite(arr[point][i])) {
-                point = i;
-                continue;
-            }
-            if (ch.get(i) && Double.isInfinite(arr[point][i])) {
-                res = false;
-                break;
-            }
-            res = true;
-        }
-        return res;
-    }
+//    public static boolean isConnected(double[][] arr, BitArray ch) {
+//        boolean res = false;
+//        int point = 0;
+//        for (int i = 1; i < arr.length; i++) {
+//            if (ch.get(i) && (arr[point][i] > 0) && !Double.isInfinite(arr[point][i])) {
+//                point = i;
+//                continue;
+//            }
+//            if (ch.get(i) && Double.isInfinite(arr[point][i])) {
+//                res = false;
+//                break;
+//            }
+//            res = true;
+//        }
+//        return res;
+//    }
 
     //selection
     public static Population selectionPop(Population oldPop) {
@@ -45,14 +45,13 @@ public class additionFunctions {
 
         while (ctrNew < newPop.length()) {
             random = Math.random()*sumOfFit;
+
             if (random > calculateFitness.arrOfFitneses[ctrOld]) {
                 newPop.ba[ctrNew] = oldPop.ba[ctrOld];
                 ctrNew++;
             } else {
                 ctrOld++;
-                if (ctrOld == oldPop.length()) {
-                    ctrOld = 0;
-                }
+                if (ctrOld == oldPop.length()) ctrOld = 0;
             }
         }
         return newPop;
@@ -78,35 +77,32 @@ public class additionFunctions {
             cutPoint = (int) (Math.random() * ((point - 2) + 1)) + 2;
 
             //choose two chromosome randomly from old population
-            p1 = oldPop.ba[(int) Math.random() * oldPop.length()];
-            p2 = oldPop.ba[(int) Math.random() * oldPop.length()];
+            p1 = oldPop.ba[(int) (Math.random() * oldPop.length())];
+            p2 = oldPop.ba[(int) (Math.random() * oldPop.length())];
 
             for (int i = 0; i < cutPoint; i++) {
                 tmpArray = p1.get(i);
                 p1.set(i, p2.get(i));
                 p2.set(i, tmpArray);
             }
-
-            if (additionFunctions.isConnected(Main.matrix, p1)) {
                 newPop.ba[newCtr] = p1;
                 newCtr++;
-            } else if (additionFunctions.isConnected(Main.matrix, p2)) {
                 newPop.ba[newCtr] = p2;
                 newCtr++;
-            }
         }
         return newPop;
     }
+
 
     //mutation function
     public static Population mutate(Population oldPop) {
         //counters
         int ctr = 0;
         //mutation level
-        double a = Math.ceil(0.04 * oldPop.length());
+        double a = Math.ceil(0.5 * oldPop.length());
 //        System.out.println(a);
         //random variables
-        int randChromo,
+        int     randChromo,
                 randGene;
         //tmp chromosome
         BitArray tmpChromo;
@@ -117,10 +113,8 @@ public class additionFunctions {
             tmpChromo = oldPop.ba[randChromo];
             tmpChromo.set(randGene, !oldPop.ba[randChromo].get(randGene));
 
-            if (additionFunctions.isConnected(Main.matrix, tmpChromo)) {
-                oldPop.ba[randChromo] = tmpChromo;
-                ctr++;
-            }
+            oldPop.ba[randChromo] = tmpChromo;
+            ctr++;
         }
         return oldPop;
     }
